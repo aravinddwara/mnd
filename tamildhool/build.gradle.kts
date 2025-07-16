b1503036f2f68cfa08363751cdb9f90fdc554e34
aravinddwara <149893688+aravinddwara@users.noreply.github.com>
2025-07-16 13:16:26 +0530

    Update build.gradle.kts

--- tamildhool/build.gradle.kts
+++ tamildhool/build.gradle.kts
@@ -1,11 +1,7 @@
 plugins {
-    kotlin("jvm") version "1.7.10"
+    id("org.jetbrains.kotlin.jvm")
 }
 
-subprojects {
-    apply(plugin = "org.jetbrains.kotlin.jvm")
-    repositories {
-        mavenCentral()
-        google()
-    }
+dependencies {
+    implementation(project(":cloudstream3"))
 }
